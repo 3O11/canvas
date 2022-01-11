b248a565fc0afb580341dc631c1697826b1060c5
Viktor Soukup <vitosoukup@gmail.com>
2022-01-11 22:14:20 +0100

    Add a few basic operators for RGB

---
+++ Common/Colour/ColourUtils.cpp
@@ -0,0 +1,31 @@
+#include "ColourUtils.h"
+
+namespace cc
+{
+	RGB operator+ (const RGB& a, const RGB& b)
+	{
+		return { a.r + b.r, a.g + b.g, a.b + b.b };
+	}
+
+	RGB& operator+= (RGB& a, const RGB& b)
+	{
+		a.r += b.r;
+		a.g += b.g;
+		a.b += b.b;
+		return a;
+	}
+
+	RGBA& operator+= (RGBA& a, const RGBA& b)
+	{
+		a.r += b.r;
+		a.g += b.g;
+		a.b += b.b;
+		a.a += b.a;
+		return a;
+	}
+
+	RGBA operator/(const RGBA& a, float s)
+	{
+		return {a.r / s, a.g / s, a.b / s, a.a / s};
+	}
+}
---
+++ Common/Colour/ColourUtils.h
@@ -0,0 +1,15 @@
+#ifndef _CC_COLOUR_UTILS_H
+#define _CC_COLOUR_UTILS_H
+
+#include "RGB.h"
+
+namespace cc
+{
+	RGB  operator+  (const RGB& a, const RGB& b);
+	RGB& operator+= (RGB& a, const RGB& b);
+
+	RGBA& operator+= (RGBA& a, const RGBA& b);
+	RGBA  operator/  (const RGBA& a, float s);
+}
+
+#endif //_CC_COLOUR_UTILS_H
